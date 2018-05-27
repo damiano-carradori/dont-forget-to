d58e8eec06f38ee0b60495ec670543b68a34889d
Damiano Carradori <damiano.carradori@gmail.com>
2018-05-28 00:52:44 +0200

    Added a control on the new task text Inserted an helper message above the list

--- src/components/DontForgetTo.js
+++ src/components/DontForgetTo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import DontForgetToItem from "./DontForgetToItem";
 import _ from 'lodash'
+import cx from 'classnames'
 
 class DontForgetTo extends Component {
     constructor() {
@@ -19,21 +20,35 @@ class DontForgetTo extends Component {
     }
 
     addTaks = e => {
-        // this.setState()
         if (e.key === 'Enter') {
-            e.preventDefault();
             e.stopPropagation();
-            let time = new Date();
-            let newTask = {
-                id: time.getTime(),
-                text: e.target.value,
-                done: false
-            };
-            this.setState({
-                tasks: [newTask, ...this.state.tasks]
-            });
+            e.preventDefault();
+
+            let text = e.target.value.trim();
+            if(text) {
+                let time = new Date();
+                let newTask = {
+                    id: time.getTime(),
+                    text: text,
+                    done: false
+                };
+                this.setState({
+                    tasks: [newTask, ...this.state.tasks]
+                });
+            }
             e.target.value = ''
         }
+    };
+
+    taskDone = id => {
+        let task = _.find(this.state.tasks, { 'id': id });
+        let newList = _.remove(this.state.tasks, task => {
+            return task.id !== id
+        });
+        task.done = true;
+        this.setState({
+            tasks: [...newList, task]
+        });
     };
 
     removeTask = id => {
@@ -42,7 +57,6 @@ class DontForgetTo extends Component {
                 return task.id !== id
             })
         });
-        console.log(id)
     };
 
     render() {
@@ -54,16 +68,26 @@ class DontForgetTo extends Component {
                     placeholder="write here and press Enter to remember"
                     onKeyDown={this.addTaks}
                 />
-                <div className="dont-forget-to-list">
+                <div className={cx(
+                    'dont-forget-to-list',
+                    { empty: !tasks.length }
+                )}>
                     {tasks.map(
                         task =>
                             <DontForgetToItem
                                 key={task.id}
                                 text={task.text}
                                 done={task.done}
+                                handleDone={() => this.taskDone( task.id )}
                                 handleRemove={() => this.removeTask( task.id )}
                             />
                     )}
+                </div>
+                <div className="total-counter">
+                    {tasks.length?
+                        `${tasks.length} task${tasks.length>1?'s':''} left`:
+                        <span>Great, you have accomplished all your tasks!<br/>Write on the box above and press ⏎ ( Enter ) to add a new task.</span>
+                    }
                 </div>
             </div>
         )
