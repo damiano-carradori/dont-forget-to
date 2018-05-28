f63125918326af42331db6418f2e4e27adce8386
Damiano Carradori <damiano.carradori@gmail.com>
2018-05-29 00:50:24 +0200

    Introduced plain Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 import DontForgetTo from "./components/DontForgetTo";
+import _ from 'lodash'
+import { createStore, combineReducers } from 'redux';
+
+const tasksReducer = ( state = [], action ) => {
+    switch (action.type) {
+        case 'ADD_TASK':
+            let newTask = {
+                id : action.id,
+                text : action.text,
+                done : false
+            };
+            return [ newTask, ...state ];
+        case 'TOGGLE_TASK':
+            let task = _.find(state, { id: action.id });
+            let newList = _.filter(state, task => task.id !== action.id );
+            task.done = !task.done;
+            return task.done?[...newList, task]:[task, ...newList];
+        case 'DELETE_TASK':
+            return _.filter(state, task => task.id !== action.id );
+        default:
+            return state
+    }
+};
+const filterReducer = ( state = 'SHOW_ALL', action ) => {
+    switch (action.type) {
+        case 'SET_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+const reducer = combineReducers({
+    tasks : tasksReducer,
+    filter : filterReducer
+});
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
+    componentDidMount() {
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        })
+
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
     render() {
         return (
             <div className="App">
@@ -10,7 +59,10 @@ class App extends Component {
                     <h1 className="App-title">Don't forget to...</h1>
                 </header>
                 <main className="App-main">
-                    <DontForgetTo/>
+                    <DontForgetTo
+                        tasks={store.getState().tasks}
+                        dispatch={store.dispatch}
+                    />
                 </main>
                 <footer>
                     &copy; 2018 - Made with <span role="img" aria-label="love">❤️</span> and <span role="img" aria-label="React">⚛️</span> by Damiano Carradori
--- src/components/DontForgetTo.js
+++ src/components/DontForgetTo.js
@@ -5,20 +5,6 @@ import DontForgetToFooter from "./DontForgetToFooter";
 import DontForgetToList from "./DontForgetToList";
 
 class DontForgetTo extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            tasks: [
-                {
-                    id: 0,
-                    text: 'First to do item',
-                    done: false
-                }
-            ]
-        }
-
-    }
 
     addTaks = e => {
         if (e.key === 'Enter') {
@@ -28,54 +14,31 @@ class DontForgetTo extends Component {
             let text = e.target.value.trim();
             if(text) {
                 let time = new Date();
-                let newTask = {
-                    id: time.getTime(),
+                this.props.dispatch({
+                    type: 'ADD_TASK',
                     text: text,
-                    done: false
-                };
-                this.setState({
-                    tasks: [newTask, ...this.state.tasks]
+                    id: time.getTime()
                 });
             }
             e.target.value = ''
         }
     };
-
-    taskDone = id => {
-        let task = _.find(this.state.tasks, { 'id': id });
-        let newList = _.remove(this.state.tasks, task => {
-            return task.id !== id
-        });
-        task.done = !task.done;
-        if(task.done) {
-            this.setState({
-                tasks: [...newList, task]
-            });
-        } else {
-            this.setState({
-                tasks: [task, ...newList]
-            });
-        }
-    };
-
-    removeTask = id => {
-        this.setState({
-            tasks: _.remove(this.state.tasks, task => {
-                return task.id !== id
-            })
-        });
-    };
-
     render() {
-        let tasks = this.state.tasks;
+        let tasks = this.props.tasks;
         let to_do = _.size(_.filter(tasks, task => !task.done ));
         return (
             <div className="dont-forget-to-container">
                 <DontForgetToAdd handleAdd={(e) => { this.addTaks(e) }}/>
                 <DontForgetToList
                     tasks={tasks}
-                    onToggleDone={ id => id}
-                    onRemove={ id => id}
+                    onToggleDone={ id => this.props.dispatch({
+                        type: 'TOGGLE_TASK',
+                        id
+                    })}
+                    onRemove={ id => this.props.dispatch({
+                        type: 'DELETE_TASK',
+                        id
+                    })}
                 />
                 <DontForgetToFooter total={to_do}/>
             </div>
