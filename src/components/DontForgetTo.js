619318c6885a1ac538a67a4bb2445ff996540591
Damiano Carradori <damiano.carradori@gmail.com>
2018-09-18 17:48:58 +0200

    Added the Drag and Drop Context using reorderTasks action on the Drag End event, and connected the component to the filter status for the reorder function

--- src/components/DontForgetTo.js
+++ src/components/DontForgetTo.js
@@ -1,19 +1,41 @@
 import React  from 'react'
+import { connect } from 'react-redux';
 import DontForgetToFilter from "./DontForgetToFilter";
 import DontForgetToAdd from "./DontForgetToAdd";
 import DontForgetToList from "./DontForgetToList";
 import DontForgetToFooter from "./DontForgetToFooter";
+import { DragDropContext } from "react-beautiful-dnd"
 import '../style/DontForgetToContainer.css'
+import { reorderTasks } from "../actionCreators";
 
-let DontForgetTo = () => {
+const mapStateToProps = state => {
+    return {
+        filter : state.filter
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onReorder: (source, destination, filter) => {
+            dispatch(reorderTasks(source, destination, filter))
+        }
+    }
+};
+
+let DontForgetTo = ({ onReorder, filter }) => {
     return (
-        <div className="dont-forget-to-container">
-            <DontForgetToAdd/>
-            <DontForgetToList/>
-            <DontForgetToFooter/>
-            <DontForgetToFilter/>
-        </div>
+        <DragDropContext onDragEnd={result => {
+            let {source, destination} = result;
+            onReorder(source, destination, filter);
+        }}>
+            <div className="dont-forget-to-container">
+                <DontForgetToAdd/>
+                <DontForgetToList/>
+                <DontForgetToFooter/>
+                <DontForgetToFilter/>
+            </div>
+        </DragDropContext>
     )
 };
 
-export default DontForgetTo
+export default connect(mapStateToProps,mapDispatchToProps)(DontForgetTo)
