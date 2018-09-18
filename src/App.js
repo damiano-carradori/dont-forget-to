b41bb92d0c2d94d766f74d0f7d6a6a6a7252b21e
Damiano Carradori <damiano.carradori@gmail.com>
2018-09-18 17:40:09 +0200

    Import Font Awesome's icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import DontForgetTo from "./components/DontForgetTo";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faTrash,faCheckCircle);
 
 class App extends Component {
     render() {
