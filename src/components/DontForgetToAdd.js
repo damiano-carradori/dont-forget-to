b4d35f193aa1a20b92c79979701debccd9d3ae70
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-04 19:55:57 +0200

    Introduced the Mutation for the creation of a new task

--- src/components/DontForgetToAdd.js
+++ src/components/DontForgetToAdd.js
@@ -2,26 +2,47 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { newTask } from '../actionCreators'
 import '../style/DontForgetToAdd.css'
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
+const ADD_TASK = gql`
+    mutation AddTask($text: String!) {
+        addTask(text: $text) {
+            _id
+            text
+            done
+        }
+    }
+`;
 
 let DontForgetToAdd = ({ dispatch }) => {
-    const addTask = e => {
-        if (e.key === 'Enter') {
-            e.stopPropagation();
-            e.preventDefault();
-            let text = e.target.value.trim();
-            if(text) {
-                dispatch(newTask(text));
-            }
-            e.target.value = ''
-        }
-    };
     return(
-        <input
-            className="dont-forget-to-add"
-            type="text"
-            placeholder="write here and press ⏎ ( Enter ) to add a new task"
-            onKeyDown={addTask}
-        />
+        <Mutation mutation={ADD_TASK}>
+            {(addTask) => (
+                <input
+                    className="dont-forget-to-add"
+                    type="text"
+                    placeholder="write here and press ⏎ ( Enter ) to add a new task"
+                    onKeyDown={ e => {
+                        if (e.key === 'Enter') {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            let text = e.target.value.trim();
+                            if(text) {
+                                addTask({ variables: { text } }).then(
+                                    response => {
+                                        let task = response.data.addTask;
+                                        dispatch(newTask(task._id,text));
+                                        console.log(task)
+                                    }
+                                );
+                            }
+                            e.target.value = ''
+                        }
+                    } }
+                />
+            )}
+        </Mutation>
     )
 };
 
