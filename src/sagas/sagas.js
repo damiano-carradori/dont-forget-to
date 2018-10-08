6444136583cb838816b842639b6d7a261de864eb
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-08 18:48:27 +0200

    Added the ADD_TASK_REQUESTED flow

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,6 +1,6 @@
 // import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 import { call, put, takeEvery } from 'redux-saga/effects'
-import { getUser } from './graphql'
+import { getUser, addTask } from './graphql'
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchUser(action) {
@@ -17,13 +17,32 @@ function* fetchUser(action) {
         yield put({type: "USER_FETCH_FAILED", message: e.message});
     }
 }
-
+function* createTask(action) {
+    if( action.user === null ){
+        let id = +new Date();
+        yield put({type: "ADD_TASK", text: action.text, id });
+    } else {
+        try {
+            let response = yield call(addTask, action.text);
+            //{_id: "5bbb871114e043006208c62b", text: "test 4", done: false, __typename: "Task"}
+            console.log(response);
+            yield put({
+                type: "ADD_TASK",
+                ...response,
+                id: response._id
+            });
+        } catch (e) {
+            yield put({type: "ADD_TASK_FAILED", message: e.message});
+        }
+    }
+}
 /*
   Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
   Allows concurrent fetches of user.
 */
 function* mySaga() {
     yield takeEvery("USER_FETCH_REQUESTED", fetchUser);
+    yield takeEvery("ADD_TASK_REQUESTED", createTask);
 }
 
 /*
