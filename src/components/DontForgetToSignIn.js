5901563acda3bec653aea908e2d0e2091f404dff
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-08 17:55:05 +0200

    Connected the component with the redux store to simulate a fake login

--- src/components/DontForgetToSignIn.js
+++ src/components/DontForgetToSignIn.js
@@ -1,13 +1,35 @@
 import React  from 'react'
+import { connect } from 'react-redux';
+import {signIn} from "../actionCreators";
 import '../style/DontForgetToSignIn.css'
 
-const DontForgetToSignIn = () => {
+const mapStateToProps = state => {
+    return {
+        user : state.user
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onSignIn: () => {
+            dispatch(signIn())
+        }
+    }
+};
+
+const DontForgetToSignIn = ({ user, onSignIn }) => {
     return (
-        <div className="dont-forget-to-sign-in">
-            <div className="user-name">Unknown</div>
-            <img src="https://www.shareicon.net/download/2015/09/18/103159_user.ico" alt="User profile image"/>
+        <div className="dont-forget-to-sign-in" onClick={()=>{
+            if(user===null){
+                onSignIn();
+            }
+        }}>
+            <div className="user-name">{user===null?'Sign in':user.username}</div>
+            <img
+                src={user===null?'https://www.shareicon.net/download/2015/09/18/103159_user.ico':user.profile_picture}
+                alt="User profile pic"/>
         </div>
     )
 };
 
-export default DontForgetToSignIn;
+export default connect(mapStateToProps,mapDispatchToProps)(DontForgetToSignIn);
