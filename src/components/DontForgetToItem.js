4d66c9d66df09fd9cf6de1a13a3eccb4a0bb6028
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-13 16:19:16 +0200

    Added filter from state

--- src/components/DontForgetToItem.js
+++ src/components/DontForgetToItem.js
@@ -8,7 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const mapStateToProps = state => {
     return {
-        user: state.user
+        user: state.user,
+        filter: state.filter
     }
 };
 
