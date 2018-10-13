ac22d224df881e9a189882310982147eeafdf144
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-13 16:04:43 +0200

    Removed the tasks filter

--- src/components/DontForgetToList.js
+++ src/components/DontForgetToList.js
@@ -1,30 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Droppable } from "react-beautiful-dnd"
-import _ from "lodash";
 import cx from 'classnames'
 import DontForgetToItem from "./DontForgetToItem";
 import '../style/DontForgetToList.css'
 
 const mapStateToProps = state => {
-    const filterTasks = ( tasks, filter ) =>{
-        switch (filter) {
-            case 'SHOW_ALL':
-                return tasks;
-            case 'SHOW_ACTIVE':
-                return _.filter(state.tasks, task => !task.done );
-            case 'SHOW_COMPLETED':
-                return _.filter(state.tasks, task => task.done );
-            default:
-                return false;
-        }
-    };
     return {
-        tasks : filterTasks(state.tasks, state.filter)
+        tasks : state.tasks
     }
 };
 
-let DontForgetToList = ({tasks}) => {
+const DontForgetToList = ({tasks}) => {
     return (
         <Droppable droppableId="dont-forget-to-list">
             {provided => (
@@ -36,11 +23,10 @@ let DontForgetToList = ({tasks}) => {
                         {empty: !tasks.length}
                     )}>
                     {tasks.map(
-                        ( task, index ) =>
+                        task =>
                             <DontForgetToItem
                                 key={task.id}
                                 {...task}
-                                index={index}
                             />
                     )}
                     {provided.placeholder}
