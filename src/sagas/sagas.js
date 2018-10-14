84e7f776965692d5c077b6a48d9e4bf843432c18
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-14 19:24:47 +0200

    Completed the reorderTasks worker using the moveTask mutation

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -60,7 +60,7 @@ function* toggleTask({id, done}) {
 function* reorderTasks({user, source, destination}) {
     // TODO: check user id before perform the edit
     try {
-        let response = yield call(GraphQL.updateTask);
+        yield call(GraphQL.moveTask, user, source.index, destination.index);
         yield put({
             type: "REORDER_TASKS",
             source,
