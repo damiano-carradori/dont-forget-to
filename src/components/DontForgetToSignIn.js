d33a0399f33b9ed3f102f975f2376bd838ac898e
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-16 21:44:30 +0200

    Changed the onClick function, now dispatch toggle the sign in side bar

--- src/components/DontForgetToSignIn.js
+++ src/components/DontForgetToSignIn.js
@@ -1,30 +1,26 @@
 import React  from 'react'
 import { connect } from 'react-redux';
-import {signIn} from "../actionCreators";
+import {toggleSignIn} from "../actionCreators";
 import '../style/DontForgetToSignIn.css'
 import userImage from '../images/user.png'
 
 const mapStateToProps = state => {
     return {
-        user : state.user
+        user : state.user.account
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onSignIn: () => {
-            dispatch(signIn())
+        openSignInSide: () => {
+            dispatch(toggleSignIn())
         }
     }
 };
 
-const DontForgetToSignIn = ({ user, onSignIn }) => {
+const DontForgetToSignIn = ({ user, openSignInSide }) => {
     return (
-        <div className="dont-forget-to-sign-in" onClick={()=>{
-            if(user===null){
-                onSignIn();
-            }
-        }}>
+        <div className="dont-forget-to-sign-in" onClick={openSignInSide}>
             <div className="user-name">{user===null?'Sign in':user.username}</div>
             <img
                 src={user===null?userImage:user.profile_picture}
