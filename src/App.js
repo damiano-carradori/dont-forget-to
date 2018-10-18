bc3401abea872242d6129a846364d509b6f34fe8
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-18 18:28:33 +0200

    Imported the times icon from Font Awesome

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import DontForgetTo from "./components/DontForgetTo";
 import DontForgetToSignIn from "./components/DontForgetToSignIn";
 import DontForgetToSignInSide from "./components/DontForgetToSignInSide";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faEllipsisV, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faTrash,faCheckCircle,faEllipsisV);
+library.add(faTrash,faCheckCircle,faEllipsisV, faTimes);
 
 class App extends Component {
     render() {
