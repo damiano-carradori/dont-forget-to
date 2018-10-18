d56f6a09c83f5325e3b5d96130c9d59f79bb1f51
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-18 19:28:52 +0200

    Added the DontForgetToSignInForm

---
+++ src/components/DontForgetToSignInForm.js
@@ -0,0 +1,46 @@
+import React, { Component }  from 'react'
+import { connect } from "react-redux"
+import {signIn, toggleSignIn} from "../actionCreators"
+import '../style/DontForgetToSignInForm.css'
+
+class DontForgetToSignInForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        let {dispatch} = this.props;
+        dispatch(signIn());
+        dispatch(toggleSignIn());
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="dont-forget-to-sign-in-form" onSubmit={this.handleSubmit}>
+                <input type="text" name="username" placeholder="Username" onChange={this.handleInputChange}/>
+                <input type="password" name="password" placeholder="Password" onChange={this.handleInputChange}/>
+                <button type="submit">Sign in</button>
+            </form>
+        );
+    }
+}
+
+export default connect()(DontForgetToSignInForm);
