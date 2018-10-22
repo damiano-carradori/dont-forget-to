73fe4220f1c729e94d3e2a09457ec1df24bd6bdc
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-23 01:05:46 +0200

    Changed the form using the Apollo Mutation

--- src/components/DontForgetToSignInForm.js
+++ src/components/DontForgetToSignInForm.js
@@ -1,7 +1,28 @@
 import React, { Component }  from 'react'
 import { connect } from "react-redux"
-import {signIn, toggleSignIn} from "../actionCreators"
+import {Mutation} from "react-apollo";
+import gql from "graphql-tag";
+import {toggleSignIn} from "../actionCreators"
 import '../style/DontForgetToSignInForm.css'
+
+const LOG_IN = gql`
+    mutation LogIn($username: String!, $password: String!) {
+        logIn(username: $username, password: $password) {
+            token
+            user{
+                id
+                username
+                profile_picture
+                tasks{
+                    id
+                    position
+                    text
+                    done
+                }
+            }
+        }
+    }
+`;
 
 class DontForgetToSignInForm extends Component {
     constructor(props) {
@@ -25,23 +46,31 @@ class DontForgetToSignInForm extends Component {
         });
     }
 
-    handleSubmit(event) {
-        let {dispatch} = this.props;
+    handleSubmit(event,signIn) {
         let {username, password} = this.state;
-        dispatch(signIn(username, password));
-        dispatch(toggleSignIn());
+        signIn({variables: {username, password}});
         event.preventDefault();
     }
 
     render() {
         return (
-            <form className="dont-forget-to-sign-in-form" onSubmit={this.handleSubmit}>
-                <input id="username" type="text" name="username" placeholder="Username" autoComplete="username" onChange={this.handleInputChange}/>
-                <label htmlFor="username">Test: Admin</label>
-                <input id="password" type="password" name="password" placeholder="Password" autoComplete="current-password" onChange={this.handleInputChange}/>
-                <label htmlFor="password">Test: 1234</label>
-                <button type="submit">Sign in</button>
-            </form>
+            <Mutation
+                mutation={LOG_IN}
+                onCompleted={(data)=> {
+                    let {dispatch} = this.props;
+                    let auth = data.logIn;
+                    dispatch(toggleSignIn());
+                    dispatch({type: "USER_LOG_IN_SUCCEEDED", auth});
+                }}>
+                {(signIn, { loading, error }) => (
+                    <form className="dont-forget-to-sign-in-form" onSubmit={(e)=>this.handleSubmit(e,signIn)}>
+                        <input className={error && "error"} id="username" type="text" name="username" placeholder="Username" autoComplete="username" onChange={this.handleInputChange}/>
+                        <input className={error && "error"} id="password" type="password" name="password" placeholder="Password" autoComplete="current-password" onChange={this.handleInputChange}/>
+                        {error && <div className="error-message">{error.message}</div>}
+                        {loading && <div className="loading-button">Loading...</div> || <button type="submit">Sign in</button>}
+                    </form>
+                )}
+            </Mutation>
         );
     }
 }
