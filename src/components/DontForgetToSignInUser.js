f43b4fe7b05fc1cb9f5e03c08e9c7688a9c30879
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-22 21:18:44 +0200

    Removed the user details from the props, replaced with a GraphQL Query

--- src/components/DontForgetToSignInUser.js
+++ src/components/DontForgetToSignInUser.js
@@ -2,33 +2,56 @@ import React  from 'react'
 import { connect } from 'react-redux'
 import {signOut} from "../actionCreators"
 import '../style/DontForgetToSignInUser.css'
+import {Query} from "react-apollo";
+import gql from "graphql-tag";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+    uri: "https://floating-reaches-16037.herokuapp.com/"
+});
+
+const GET_ME = gql`
+    {
+        me{
+            id
+            username
+            profile_picture
+        }
+    }
+`;
 
 const mapStateToProps = state => {
     return {
-        token: state.user.token,
-        user: state.user.account
+        token: state.user.token
     }
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onSignOut: () => {
-            dispatch(signOut())
-        }
-    }
-};
-
-const DontForgetToSignInUser = ({ token, user, onSignOut }) => {
+const DontForgetToSignInUser = ({ token, dispatch }) => {
     return (
-        <div className="dont-forget-to-sign-in-user">
-            <img
-                src={user===null?'':user.profile_picture}
-                alt="User profile pic"/>
-            <div className="user-name">{user===null?'Sign in':user.username}</div>
-            <button>Sign Out</button>
-
-        </div>
+        <Query
+            query={GET_ME}
+            client={client}
+            context={{
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            }}>
+            {({loading, error, data}) => {
+                if (loading) return "Loading...";
+                if (error) return `Error: ${error.message}`;
+                let {me} = data;
+                return (
+                    <div className="dont-forget-to-sign-in-user">
+                        <img
+                            src={me.profile_picture}
+                            alt="User profile pic"/>
+                        <div className="user-name">{me.username}</div>
+                        <button onClick={() => dispatch(signOut())}>Sign Out</button>
+                    </div>
+                );
+            }}
+        </Query>
     )
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(DontForgetToSignInUser);
+export default connect(mapStateToProps)(DontForgetToSignInUser);
