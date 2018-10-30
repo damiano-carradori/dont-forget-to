27a0f8ac3f6badafee426968a64ec9800c8954e6
Damiano Carradori <damiano.carradori@gmail.com>
2018-10-30 15:00:48 +0100

    Replaced Redux with Apollo state

--- src/components/DontForgetToSignIn.js
+++ src/components/DontForgetToSignIn.js
@@ -1,32 +1,28 @@
-import React  from 'react'
-import { connect } from 'react-redux';
-import {toggleSignIn} from "../actionCreators";
-import '../style/DontForgetToSignIn.css'
-import userImage from '../images/user.png'
+import React  from "react"
+import {Query} from "react-apollo"
+import gql from "graphql-tag";
+import userImage from "../images/user.png"
+import "../style/DontForgetToSignIn.css"
 
-const mapStateToProps = state => {
-    return {
-        user : state.user.account
+const GET_USER = gql`
+    {
+        user @client
     }
-};
+`;
 
-const mapDispatchToProps = dispatch => {
-    return {
-        openSignInSide: () => {
-            dispatch(toggleSignIn())
-        }
-    }
-};
-
-const DontForgetToSignIn = ({ user, openSignInSide }) => {
+const DontForgetToSignIn = (props) => {
     return (
-        <div className="dont-forget-to-sign-in" onClick={openSignInSide}>
-            <div className="user-name">{user===null?'Sign in':user.username}</div>
-            <img
-                src={user===null?userImage:user.profile_picture}
-                alt="User profile pic"/>
-        </div>
+        <Query query={GET_USER}>
+            {({ data: { user }, client }) => (
+                <div className="dont-forget-to-sign-in" onClick={() => client.writeData({ data: { side: true } })}>
+                    <div className="user-name">{user===null?'Sign in':user.username}</div>
+                    <img
+                        src={user===null?userImage:user.profile_picture}
+                        alt="User profile pic"/>
+                </div>
+            )}
+        </Query>
     )
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(DontForgetToSignIn);
+export default DontForgetToSignIn;
